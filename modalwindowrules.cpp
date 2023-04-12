ddfc231694b0a2a59cc531a73eaba321eaebffe3
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-04-12 20:01:38 +0300

    Changed rules

--- modalwindowrules.cpp
+++ modalwindowrules.cpp
@@ -6,11 +6,26 @@ ModalWindowRules::ModalWindowRules(QWidget *parent) :
     ui(new Ui::ModalWindowRules)
 {
     ui->setupUi(this);
+    load_text();
     ui->textBrowser->setText(text);
     setMinimumSize(size());
+    setMaximumWidth(size().width());
 }
 
 ModalWindowRules::~ModalWindowRules()
 {
     delete ui;
 }
+
+void ModalWindowRules::load_text()
+{
+    QString fileName(":/resources/text.html");
+
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly))
+        text = "<h1>Ошибка загрузки справки</h1>";
+    else
+        text = file.readAll();
+    file.close();
+}
+
